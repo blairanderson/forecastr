4a3ad51ba623d6f852d04e0ce0b3ef44452f739e
Blair Anderson <blair81@gmail.com>
2022-04-27 09:57:33 -0700

    add docs

--- app.rb
+++ app.rb
@@ -4,7 +4,49 @@ require 'prophet'
 require 'date'
 
 get '/' do
-  return "Hello from Sinatra on Heroku!"
+  docs = <<-HEREDOC
+// first get a series of data that you want to forecast
+series = Orders.group_by_day(:created_at).sum("quantity_shipped")
+// it should look like
+// series = {
+//   Date.parse("2020-01-01") => 100,
+//   Date.parse("2020-01-02") => 150,
+//   Date.parse("2020-01-03") => 136,
+//   ...
+// }
+
+// Then POST it.
+uri = URI("https://forecast.shipmentbot.com/forecast")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+request.body = {
+    'data' => data.as_json,
+    'count' => 100
+}.to_json
+response = http.request(request).body
+HEREDOC
+
+return erb("
+<!doctype html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>ShipmentBot Forecasting</title>
+<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+</head>
+<body>
+<main class='container'>
+  <h1>Hello from ShipmentBot Forecasting!</h1>
+  <p>Try this Code:</p>
+  <pre>
+  <code>
+  #{docs}
+  </code>
+  </pre>
+</main>
+</body>
+</html>")
 end
 
 
