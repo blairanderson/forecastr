783cd0101e78604960e43d148136f4a4f5c72586
Blair Anderson <blair81@gmail.com>
2022-04-27 08:51:51 -0700

    mvp

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
-require 'rubygems'
 require 'sinatra'
+require 'json'
 
 get '/' do
   "Hello from Sinatra on Heroku!"
+end
+
+
+get '/forecast' do
+  content_type :json
+  { song: "Wake me Up" }.to_json
 end
